5f8ec14d15dc35852c2e50db8b0aea05a21059ff
Tnifey <tnifey@gmail.com>
2017-09-24 19:50:22 +0200

    File, json, json5 and svg loaders
    .json .json5 .svg and various file extensions like images, fonts, audio, video and more

--- webpack.config.js
+++ webpack.config.js
@@ -77,10 +77,78 @@ module.exports = {
 				]
 			},
 
+			/* svg inline loader - svg */
+			{
+				test: /\.svg$/,
+				loader: "svg-inline-loader"
+			},
+
+			/* json loader - .json */
+			{
+				test: /\.json$/,
+				loader: "json-loader"
+			},
+
+			/* json5 loader - .json5 */
+			{
+				test: /\.json5$/,
+				loader: "json5-loader"
+			},
+
 			/* raw text */
 			{
 				test: /\.(txt|raw)$/,
 				loader: "raw-loader",					
+			},
+
+			/* fonts loader */
+			{
+				test: /\.(ttf|woff2?|otf|fnt|eot)$/,
+				loader: "file-loader",
+				options: {
+					name: "[hash].[ext]",
+					outputPath: "assets/fonts",
+				}
+			},
+
+			/* images loader */
+			{
+				test: /\.(bmp|gif|ico|jpe?g|png|tiff?|webp)$/,
+				loader: "file-loader",
+				options: {
+					name: "[hash].[ext]",
+					outputPath: "assets/images/",
+				}
+			},
+
+			/* audio loader */
+			{
+				test: /\.(wav|mp3|ogg|flac|acc)$/,
+				loader: "file-loader",
+				options: {
+					name: "[hash].[ext]",
+					outputPath: "assets/audios/",
+				}
+			},
+
+			/* video loader */
+			{
+				test: /\.(mp4|webm|ogv|wmv)$/,
+				loader: "file-loader",
+				options: {
+					name: "[hash].[ext]",
+					outputPath: "assets/videos/",
+				}
+			},
+
+			/* file loader */
+			{
+				test: /\.(7z|arj|deb|pkg|rar|rpm|gz|z|zip|bin|dmg|iso|toast|vcd|apk|bat|bin|cgi|pl|com|exe|gadget|jar|py|wsf|docx?|odt|pdf|rtf|tex|odp|pp[st]|pptx)$/,
+				loader: "file-loader",
+				options: {
+					name: "[hash].[ext]",
+					outputPath: "assets/files/",
+				}
 			},
 		]
 	},
